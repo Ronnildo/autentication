86eecbeac2d6319943b58913df4a46d7f25f7fbb
Ronnildo <ronildosp03@gmail.com>
2022-12-24 14:47:39 -0300

    Add arquivos para firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:autentication/app/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'firebase_options.dart';
 import 'app/register.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Autetication App",
       // modifica pra /register, mas quanto for da um git add muda pra /login
-      initialRoute: "/register",
+      initialRoute: "/login",
       routes: {
         "/register": (context) => const Register(),
         "/login": (context) => const Login(),
